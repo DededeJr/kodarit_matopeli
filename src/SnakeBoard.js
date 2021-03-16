6a35a363bc18e420c02322ec3d403d9985ee3a8b
joku <email@lol.lol>
2021-03-16 18:21:25 +0200

    mato liikkuu nuolilla

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -14,19 +14,37 @@ const SnakeBoard = () => {
   }
   const [rows, setRows] = useState(initialRows);
   const [snake, setSnake] = useState([{x: 2, y: 5}]);
-  const [direction, setDirection] = useState("right")
+  const [direction, setDirection] = useState("right");
 
-  const displayRows = rows.map(row => (
-    <div className="Snake-row">
-      {row.map(tile =>
-        <div className={`tile ${tile}`} />
-      )}
+  const changeDirectionWithKeys = e => {
+    const {keyCode} = e;
+    switch (keyCode) {
+      case 37:
+        setDirection("left");
+        break;
+      case 38:
+        setDirection("top");
+        break;
+      case 39:
+        setDirection("right");
+        break;
+      case 40:
+        setDirection("bottom");
+        break;
+    }
+  };
+  document.addEventListener("keydown", changeDirectionWithKeys);
+  const displayRows = rows.map((row, i) => (
+    <div className="Snake-row" key={i}>
+      {row.map((tile, j) => (
+        <div className={`tile ${tile}`} key={j} />
+      ))}
     </div>
   ));
 
   const displaySnake = () => {
-    const newRows = initialRows
-    snake.forEach((tile) => {
+    const newRows = initialRows;
+    snake.forEach(tile => {
       newRows[tile.x][tile.y] = "snake";
     });
     setRows(newRows);
@@ -34,10 +52,19 @@ const SnakeBoard = () => {
 
   const moveSnake = () => {
     const newSnake = [];
-    switch(direction) {
-      case'right':
-        newSnake.push({x: snake[0].x, y: (snake[0].y +1) % width});
-        break
+    switch (direction) {
+      case "right":
+        newSnake.push({x: snake[0].x, y: (snake[0].y + 1) % width});
+        break;
+      case "left":
+        newSnake.push({x: snake[0].x, y: (snake[0].y - 1 + width) % width});
+        break;
+      case "top":
+        newSnake.push({x: (snake[0].x - 1 + height) % height, y: snake[0].y});
+        break;
+      case "bottom":
+        newSnake.push({x: (snake[0].x + 1) % height, y: snake[0].y});
+        break;
       default:
         break;
     }
@@ -45,7 +72,7 @@ const SnakeBoard = () => {
     displaySnake();
   };
 
-  useInterval(moveSnake,250);
+  useInterval(moveSnake, 250);
 
   return <div className="Snake-board">{displayRows}</div>;
 };
